8413c2661dfa72c86e421f053dc8ace17316df20
Alessandro Bianco <mydevhero+github@gmail.com>
2026-01-27 22:45:44 +0100

    Refactor frontend setup script for pnpm usage
    Updated the setup script to use 'pnpm exec' for creating the Next.js app and removed commented-out lines for clarity.

--- bin/setup_frontend.sh
+++ bin/setup_frontend.sh
@@ -15,11 +15,11 @@ source "$SCRIPT_DIR/configure_env.sh"
 
 cd "$PROJECT_DIR"
 
-pnpm install -no-frozen-lockfile
+# pnpm install -no-frozen-lockfile
 
 # Inizia creando il frontend
 #   pnpm exec npx create-next-app@latest frontend
-pnpm npx create-next-app@latest frontend \
+pnpm exec npx create-next-app@latest frontend \
   --typescript \
   --tailwind \
   --eslint \
@@ -32,7 +32,7 @@ pnpm npx create-next-app@latest frontend \
 cd "$FRONTEND_DIR"
 
 # Pulizia
-# rm pnpm-lock.yaml
+rm pnpm-lock.yaml
 
 # Collega il frontend al workspace e creerà un unico file di lock nella root, ottimizzando tutto.
 #pnpm install # sulla root non in frontend
