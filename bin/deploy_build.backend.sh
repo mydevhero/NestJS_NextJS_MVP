205fba566f82f89d4a6338cd74d3a041bfa600a6
Alessandro Bianco <mydevhero+github@gmail.com>
2026-01-28 04:23:25 +0100

    Update deploy_build.backend.sh

--- bin/deploy_build.backend.sh
+++ bin/deploy_build.backend.sh
@@ -5,4 +5,4 @@ PROJECT_DIR="$SCRIPT_DIR/.."
 
 cd "${PROJECT_DIR}" || exit 1;
 
-pnpm install --ignore-workspace && ./bin/setup_backend_init.sh && pnpm --filter backend install && ./bin/setup_backend.sh && pnpm --filter backend build
+pnpm install --ignore-workspace && ./bin/setup_backend_init.sh && pnpm --filter backend install --no-frozen-lockfile && ./bin/setup_backend.sh && pnpm --filter backend build
