c4e9b591c4be07a0ed076130eb511f416713dcae
Alessandro Bianco <mydevhero@gmail.com>
2026-01-28 04:00:33 +0100

    Added deploy build scipts
    - bin/deploy_build.backend.sh
    - bin/deploy_build.frontend.sh

---
+++ bin/deploy_build.backend.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+PROJECT_DIR="$SCRIPT_DIR/.."
+
+cd "${PROJECT_DIR}" || exit 1;
+
+pnpm install --ignore-workspace
+
+./bin/setup_backend_init.sh
+
+pnpm --filter backend install
+
+./bin/setup_backend.sh
+
+pnpm --filter backend build
