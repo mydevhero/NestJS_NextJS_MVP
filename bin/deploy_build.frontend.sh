f7571678ef95c6b316042be19552b23f2de39eb4
Alessandro Bianco <mydevhero@gmail.com>
2026-01-28 04:10:23 +0100

    Updated deploy build script

--- bin/deploy_build.frontend.sh
+++ bin/deploy_build.frontend.sh
@@ -5,12 +5,4 @@ PROJECT_DIR="$SCRIPT_DIR/.."
 
 cd "${PROJECT_DIR}" || exit 1;
 
-pnpm install --ignore-workspace
-
-./bin/setup_frontend_init.sh
-
-pnpm --filter frontend install
-
-./bin/setup_frontend.sh
-
-pnpm --filter frontend build
+pnpm install --ignore-workspace && ./bin/setup_frontend_init.sh && pnpm --filter frontend install && ./bin/setup_frontend.sh && pnpm --filter frontend build
